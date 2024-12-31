f5cfad10a4f8b5f3e352a8f70f55c5efe923e826
david suico <dale.alexis96@gmail.com>
2024-12-31 10:07:55 +0800

    questions seeder and factory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
@@ -16,8 +17,14 @@ class QuestionFactory extends Factory
      */
     public function definition(): array
     {
+        $title = rtrim(fake()->sentence(rand(5, 10)), '.');
         return [
-            //
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'body' => fake()->paragraphs(rand(3, 7), true),
+            'views' => rand(0, 10),
+            'answers' => rand(0, 10),
+            'votes' => rand(-3, 10)
         ];
     }
 }
