87ea7c96be7218df363e47ccce3603a69c4b54d2
david suico <dale.alexis96@gmail.com>
2024-12-31 09:36:14 +0800

    questions table schema

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function questions() {
+        return $this->hasMany(Question::class);
+    }
 }
